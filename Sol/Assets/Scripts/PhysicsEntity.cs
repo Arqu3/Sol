8634065b09c2d806ad94dd2ccaea0c14db98dfcb
Ludwig Gustavsson <ludwiggustavsson3@gmail.com>
2017-02-28 08:03:57 +0100

    Updated movement

---
+++ Sol/Assets/Scripts/PhysicsEntity.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicsEntity : Entity
+{
+    public override void MovementUpdate()
+    {
+        Debug.LogError("PhysicsEntity base class does not have an implementation for movementupdate!");
+    }
+
+    protected RaycastHit2D DrawCast(Vector2 position, Vector2 direction, float distance)
+    {
+        RaycastHit2D hit = Physics2D.Raycast(position, direction, distance);
+        Color col = hit ? Color.green : Color.red;
+        Debug.DrawRay(position, direction * distance, col);
+        return hit;
+    }
+
+    protected RaycastHit2D DrawCast(Vector2 position, Vector2 direction, float distance, int layermask)
+    {
+        RaycastHit2D hit = Physics2D.Raycast(position, direction, distance, layermask);
+        Color col = hit ? Color.green : Color.red;
+        Debug.DrawRay(position, direction * distance, col);
+        return hit;
+    }
+}
--- Sol/Assets/Scripts/Player.cs
+++ Sol/Assets/Scripts/Player.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D), typeof(Collider2D))]
-public class Player : Entity
+public class Player : PhysicsEntity
 {
     //Public vars
     [Header("Movement variables")]
@@ -14,9 +14,28 @@ public class Player : Entity
     [Range(0.1f, 100.0f)]
     public float m_JumpForce = 20.0f;
 
+    [Header("Dash variables")]
+    [Range(0.1f, 10.0f)]
+    public float m_PauseTime = 2.0f;
+
+    [Header("Raycast layers")]
+    public LayerMask m_GroundMask;
+    public LayerMask m_WallMask;
+
     //Component vars
     private Rigidbody2D m_Rigidbody;
     private Collider2D m_Collider;
+
+    //Air vars
+    private bool m_Grounded = false;
+    private bool m_Air = false;
+
+    //Movement vars
+    private float m_Horizontal = 0.0f;
+
+    //Dash vars
+    private bool m_IsPaused = false;
+    private float m_PauseTimer = 0.0f;
 
     void Awake()
     {
@@ -28,35 +47,91 @@ public class Player : Entity
     {
         MovementUpdate();
         JumpUpdate();
+        DashUpdate();
 	}
 
     void JumpUpdate()
     {
-        bool ground = OnGround();
-        bool inAir = m_Rigidbody.velocity.y > 0;
-        if (Input.GetKey(KeyCode.Space) && ground && !inAir)
+        m_Grounded = GroundCheck();
+        m_Air = m_Rigidbody.velocity.y > 0;
+        if (Input.GetKey(KeyCode.Space) && m_Grounded && !m_Air)
             m_Rigidbody.AddForce(Vector2.up * m_JumpForce, ForceMode2D.Impulse);
     }
 
-    bool OnGround()
+    void DashUpdate()
     {
-        bool ground = false;
-        Color col = Color.red;
-        float dist = 0.6f;
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector3.down, dist);
-        if (hit)
+        m_IsPaused = Input.GetMouseButton(1);
+        if (m_IsPaused)
         {
-            col = Color.green;
-            ground = true;
+            m_Rigidbody.gravityScale = 0.0f;
         }
-
-        Debug.DrawRay(transform.position, Vector3.down * dist, col);
-
-        return ground;
+        else
+            m_Rigidbody.gravityScale = 1.0f;
     }
 
     public override void MovementUpdate()
     {
-        m_Rigidbody.velocity = new Vector2(Input.GetAxis("Horizontal") * m_Speed, m_Rigidbody.velocity.y);
+        if (!WallCheck())
+            m_Horizontal = Input.GetAxis("Horizontal");
+
+        m_Rigidbody.velocity = new Vector2(m_Horizontal * m_Speed, m_Rigidbody.velocity.y);
+    }
+
+    bool GroundCheck()
+    {
+        float dist = 0.6f;
+        Vector2 pos = m_Collider.bounds.center;
+        pos.y -= m_Collider.bounds.size.y / 1.9f;
+        pos.x -= dist / 2.0f;
+        RaycastHit2D hit = DrawCast(pos, Vector2.right, dist, m_GroundMask);
+        return hit;
+    }
+
+    bool WallCheck()
+    {
+        float x = 0.0f;
+        float dist = 0.6f;
+        bool hit = false;
+        for (int i = 0; i < 2; i++)
+        {
+            switch(i)
+            {
+                case 0:
+                    x = m_Collider.bounds.size.x;
+                    break;
+
+                case 1:
+                    x *= -1.0f;
+                    break;
+            }
+
+            Vector2 pos = m_Collider.bounds.center;
+            pos.x -= x / 1.9f;
+            pos.y -= dist / 2.0f;
+
+            RaycastHit2D rHit = DrawCast(pos, Vector2.up, dist, m_WallMask);
+
+            if (rHit)
+            {
+                float horizontal = Input.GetAxis("Horizontal");
+                if (i == 0)
+                {
+                    if (horizontal < 0.0f)
+                        m_Horizontal = 0.0f;
+                    else if (horizontal > 0.0f)
+                        m_Horizontal = horizontal;
+                }
+                else if (i == 1)
+                {
+                    if (horizontal > 0.0f)
+                        m_Horizontal = 0.0f;
+                    else if (horizontal < 0.0f)
+                        m_Horizontal = horizontal;
+                }
+
+                hit = true;
+            }
+        }
+        return hit;
     }
 }
