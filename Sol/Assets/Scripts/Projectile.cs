830f49d3b27eacdb63dbae7c27c96f95d161387e
Ludwig Gustavsson <ludwiggustavsson3@gmail.com>
2017-03-02 00:26:08 +0100

    Projectiles now spawn particles on hit

--- Sol/Assets/Scripts/Projectile.cs
+++ Sol/Assets/Scripts/Projectile.cs
@@ -9,8 +9,21 @@ public class Projectile : MonoBehaviour
     [Range(0, 100)]
     public int m_Damage = 1;
 
+    [Header("On death particles")]
+    public GameObject m_ParticlePrefab;
+
     void OnCollisionEnter2D(Collision2D col)
     {
+        if (m_ParticlePrefab)
+        {
+            GameObject particles = (GameObject)Instantiate(m_ParticlePrefab, col.contacts[0].point, Quaternion.identity);
+            Vector2 normal = col.contacts[0].normal;
+            particles.transform.rotation = Quaternion.FromToRotation(Vector3.forward, normal);
+            ProjectileParticles pp = particles.GetComponent<ProjectileParticles>();
+            if (pp)
+                pp.SetColor(GetComponent<SpriteRenderer>().color);
+        }
+
         Destroy(gameObject);
     }
 }
---
+++ Sol/Assets/Scripts/ProjectileParticles.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(ParticleSystem))]
+public class ProjectileParticles : MonoBehaviour
+{
+
+    //Component vars
+    private ParticleSystem m_System;
+
+	void Awake()
+    {
+        m_System = GetComponent<ParticleSystem>();
+        Destroy(gameObject, 1.0f);
+    }
+
+    public void SetColor(Color col)
+    {
+        var module = m_System.main;
+        module.startColor = col;
+    }
+}
