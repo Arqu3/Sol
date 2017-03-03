5e83677b072d9d46334cd970b6ce1e5166afb139
Ludwig Gustavsson <ludwiggustavsson3@gmail.com>
2017-03-03 12:42:31 +0100

    Updated enemy AI

--- Sol/Assets/Scripts/Enemy.cs
+++ Sol/Assets/Scripts/Enemy.cs
@@ -7,6 +7,8 @@ public class Enemy : PhysicsEntity
     [Header("Movement variables")]
     [Range(1.0f, 100.0f)]
     public float m_Speed = 10.0f;
+    public float m_WaitTime = 1.0f;
+    public float m_Acceleration = 2.0f;
 
     [Header("Raycast layermasks")]
     public LayerMask m_GroundMask;
@@ -16,6 +18,9 @@ public class Enemy : PhysicsEntity
     private bool m_Hit = false;
     private bool m_Grounded = false;
     private int m_Direction = 1;
+    //private bool m_Waiting = false;
+    private float m_Acc = 0.0f;
+    private bool[] m_Bools;
 
     protected override void Awake()
     {
@@ -25,6 +30,9 @@ public class Enemy : PhysicsEntity
             m_Direction = 1;
         else
             m_Direction = -1;
+
+        m_Bools = new bool[1];
+        m_Bools[0] = false;
     }
 
     void Update()
@@ -42,11 +50,31 @@ public class Enemy : PhysicsEntity
 
         if (m_Grounded)
         {
-            m_Rigidbody.velocity = new Vector2(m_Direction * m_Speed, m_Rigidbody.velocity.y);
+            if (!m_Bools[0])
+            {
+                m_Rigidbody.velocity = new Vector2(m_Direction * m_Speed * m_Acc, m_Rigidbody.velocity.y);
+                m_Acc = Mathf.Lerp(m_Acc, 1.0f, m_Acceleration * Time.deltaTime);
+            }
 
             if (m_Hit)
+            {
                 m_Direction *= -1;
+                m_Acc = 0.0f;
+                StartCoroutine(WaitForTime(m_WaitTime, 0));
+            }
         }
+    }
+
+    IEnumerator WaitForTime(float time, int boolIndex)
+    {
+        float timer = 0.0f;
+        m_Bools[boolIndex] = true;
+        while(timer < time)
+        {
+            timer += Time.deltaTime;
+            yield return null;
+        }
+        m_Bools[boolIndex] = false;
     }
 
     public override void OnHit()
